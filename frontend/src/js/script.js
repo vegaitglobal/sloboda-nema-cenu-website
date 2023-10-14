19c900cf720c28eb712238df1a8286bc50243f9c
Nemanja Sobo <n.sobo@vegaitsourcing.rs>
2023-10-14 10:23:53 +0200

    New fronted.

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,26 +1,9 @@
-//slider related news
-import Swiper from 'swiper';
-var init = false;
-var swiper;
-function swiperCard() {
-  if (window.innerWidth <= 768) {
-    if (!init) {
-      init = true;
-      swiper = new Swiper('.js-swiper', {
-        direction: 'horizontal',
-        slidesPerView: 1,
-        centeredSlides: true,
-        spaceBetween: 10
-      });
-    }
-  } else if (init) {
-    swiper.destroy();
-    init = false;
-  }
-}
-swiperCard();
-window.addEventListener('resize', swiperCard);
-// scroll text
+import slider from './slider';
+slider.init();
+
+import header from './header';
+header.init();
+
 import backgroundText from './background-text';
 backgroundText.init();
 
